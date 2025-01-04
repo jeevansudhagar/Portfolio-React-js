075161d2b6cfaf76ee748b31104cf153cb962b5e
Jeevan sudhagar A <143312457+jeevansudhagar@users.noreply.github.com>
2025-01-04 15:49:24 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,113 @@
+const logotext = "JEEVAN SUDHAGAR";
+const meta = {
+    title: "Jeeavn sudhagar",
+    description: "I’m Jeevan sudhagar data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Jeevan sudhagar",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "",
+    backgroundImage:require("../src/assets/images/bg.jpg")
+};
+
+const dataabout = {
+    title: " about my self",
+    aboutme: " I'm Jeeva from Dindigul, Tamil Nadu. I hold an MCA from Dr. Mahalingam College of Engineering and Technology, with a passion for building efficient and user-friendly web applications.",
+};
+const worktimeline = [{
+        jobtitle: "Software developer / INTERNSHIP",
+        where: "Dindigul",
+        date: "2023",
+    },
+   
+];
+
+const skills = [{
+        name: "React JS",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "Bootstrap",
+        value: 90,
+    },
+    {
+        name: "Tailwind",
+        value: 90,
+    },
+    {
+        name: "Node JS",
+        value: 50,
+    },
+    {
+        name: "Mysql",
+        value: "70"
+    }
+    
+];
+
+const services = [
+    // {
+    //     title: "UI & UX Design",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    // },
+    // {
+    //     title: "Mobile Apps",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    // },
+    // {
+    //     title: "Wordpress Design",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    // },
+];
+
+const dataportfolio = [{
+        img:require("../src/assets/images/hotel.jpg"),
+        description: "The Hotel Management.",
+        link: "https://github.com/jeevansudhagar/recipe-app-v1.git",
+    },
+    {
+        img:require("../src/assets/images/mobile.jpg"),
+        description: "Mobile shop.",
+        link: "https://github.com/jeevansudhagar/Mobile-shop.git",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "jeevansudhagar@gmail.com",
+    YOUR_FONE: "9360043610",
+    // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_n5onxj2",
+    YOUR_TEMPLATE_ID: "template_pnvpwnm",
+    YOUR_USER_ID: "9Zqq1hcimkugJn0SF",
+};
+
+const socialprofils = {
+    github: "https://github.com/jeevansudhagar",
+
+    linkedin: "https://www.linkedin.com/in/jeevan-sudhagar-9b7286224/",
+    twitter: "https://x.com/JeevanSudhagar",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
